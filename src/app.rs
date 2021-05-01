b2f53d52fad0246a0197acb5082cd2dd989ee6a1
Thai Duong Vu <vu.t.duong01@gmail.com>
2021-04-30 22:26:48 -0400

    Add basic window management

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,9 @@
-extern crate glfw;
-use glfw::Context;
+extern crate glutin;
+
+use glutin::event::{Event, WindowEvent};
+use glutin::event_loop::{ControlFlow, EventLoop};
+use glutin::window::WindowBuilder;
+use glutin::ContextBuilder;
 
 // Default values for window initialization
 const DEFAULT_WIDTH: u32 = 800;
@@ -14,7 +18,7 @@ pub struct App {
 
 impl App {
     // Default constructor to initialize App
-    pub fn new() -> Self {
+    pub fn new<'a>() -> Self {
         Self {
             width: DEFAULT_WIDTH,
             height: DEFAULT_HEIGHT,
@@ -32,33 +36,72 @@ impl App {
         self.height = height;
     }
 
+    // Set App screen width and height
+    pub fn set_size(&mut self, width: u32, height: u32) {
+        self.width = width;
+        self.height = height;
+    }
+
     // Set App screen title
     pub fn set_title(&mut self, title: &str) {
         self.title = String::from(title);
     }
 
     // Run App
-    pub fn run(&mut self, init: Option<fn(&mut App)>) {
+    pub fn run<'a>(
+        mut self,
+        init: Option<fn(&mut App)>,
+        update: Option<fn(&mut App)>,
+        render: Option<fn(&mut App)>,
+        exit: Option<fn(&mut App)>,
+    ) {
         // User-defined initialization
         if let Some(init) = init {
-            init(self);
+            init(&mut self);
         }
-        // Initialize glfw and create a window
-        let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-        let (mut glfw_window, _events) = glfw
-            .create_window(
-                self.width,
-                self.height,
-                &self.title,
-                glfw::WindowMode::Windowed,
-            )
-            .expect("Failed to create GLFW window.");
 
-        glfw_window.set_key_polling(true);
-        glfw_window.make_current();
+        // Create event loop and window builder
+        let event_loop = EventLoop::new();
+        let window_builder = WindowBuilder::new()
+            .with_title(&self.title)
+            .with_inner_size(glutin::dpi::PhysicalSize::new(self.width, self.height));
+
+        // Create context for current window
+        let windowed_context = ContextBuilder::new()
+            .build_windowed(window_builder, &event_loop)
+            .unwrap();
+        let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+
         // Main program loop
-        while !glfw_window.should_close() {
-            glfw.poll_events();
-        }
+        event_loop.run(move |event, _, control_flow| {
+            *control_flow = ControlFlow::Wait;
+            // User-defined update
+            if let Some(update) = update {
+                update(&mut self);
+            }
+            // Poll for events in main loop
+            match event {
+                Event::LoopDestroyed => {
+                    // User-defined exit
+                    if let Some(exit) = exit {
+                        exit(&mut self);
+                    }
+                    return;
+                }
+                Event::WindowEvent { event, .. } => match event {
+                    // WindowEvent::Resized(physical_size) => windowed_context.resize(physical_size),
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    _ => (),
+                },
+                Event::RedrawRequested(_) => {
+                    // User-defined render
+                    if let Some(render) = render {
+                        render(&mut self);
+                    }
+                    windowed_context.swap_buffers().unwrap();
+                }
+                _ => (),
+            }
+        });
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+extern crate sidekick;
+use sidekick::app::App;
+
+fn main() {
+    let app = App::new();
+    app.run(None, None, None, None);
+}
