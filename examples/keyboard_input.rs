6b26020ec2cf5b518159f7736b9172eda77557a9
Duong Vu <vu.t.duong01@gmail.com>
2021-07-29 21:19:21 -0400

    Create keyboard_input.rs

---
+++ examples/keyboard_input.rs
@@ -0,0 +1,16 @@
+use sidekick::app::App;
+
+fn main() {
+    // Create a sidekick app
+    let app = App::new();
+    
+    // Initialize app before first frame update
+    // Note: Dynamic environment should be initialized outside of init
+    let init = move |_app: &mut App| {};
+    
+    // Update and render game objects every frame
+    let update = move |app: &mut App| {};
+
+    // Run app
+    app.run(init, update);
+}
